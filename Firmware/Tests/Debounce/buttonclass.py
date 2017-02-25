47fe5d3138b5ea7c5a565bce9088e641e420341d
Daniel Rojas <github@danielrojas.net>
2017-02-25 11:33:44 +0100

    Better debouncing, still needs diffferentiation between pressed/released

--- Firmware/Tests/Debounce/buttonclass.py
+++ Firmware/Tests/Debounce/buttonclass.py
@@ -5,48 +5,67 @@ import micropython
 micropython.alloc_emergency_exception_buf(100)
 
 class button_debounce:
-    def __init__(self, pin_number, debounce_time=0.1):
+    def __init__(self, pin_number, debounce_down=0.05, debounce_up=0.10):
         self.pin = Pin(pin_number, Pin.IN, Pin.PULL_UP)
-        self.debounce_time = debounce_time
+        self.debounce_down = debounce_down
+        self.debounce_up = debounce_up
         self.clicked = False
+        self.official = 0
+        self.lastchecked = 0
+        self.lastdown = 0
+        self.lastup = 0
         self.counter = 0
         self.ticks   = 0
         self.pin.irq (trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=lambda p: self.edge())
 
-    def wasclicked(self):
-        if self.clicked == True:
-            ret = True
-            self.clicked = False
-        else:
-            ret = False
-        return ret
-
     def edge(self):
         now = time.ticks_ms()
         if self.pin.value() == 0: # pressed
-            diff = now - self.ticks # time.ticks_diff(now, self.ticks_down)
-            if diff > self.debounce_time * 1000.0: # success!
-                self.ticks = now
-                self.clicked = True
-                self.counter += 1
+            self.lastdown = now
         else: # released
-            self.ticks = now # reset debounce
-            self.clicked = False
-        
+            self.lastup = now
+
+    def check(self):
+        now = time.ticks_ms()
+        if self.pin.value() == 0:
+            if now - self.lastdown > self.debounce_down * 1000.0:
+                self.official = 1
+        else:
+            if now - self.lastup   > self.debounce_up * 1000.0:
+                self.official = 0
+        return self.official
+
+    def changed(self):
+        self.check()
+        if self.lastchecked == self.official:
+            return False
+        else:
+            self.lastchecked = self.official
+            return True
+
+    def lasttransition(self):
+        # if was pressed: 1
+        # if was released: -1
+        # if nothing: 0
+        pass
+
+
 button_1 = button_debounce(0)
 button_2 = button_debounce(4)
 button_3 = button_debounce(5)
 
 where = 0
 while True:
-    if button_1.wasclicked():
-        if where > 0:
-            where -= 1
-        print(where)
-    if button_2.wasclicked():
-        print(where)
-    if button_3.wasclicked():
-        if where < 10:
-            where += 1
-        print(where)
- 
+    if button_1.changed():
+        if button_1.official == 1:
+            if where > 0:
+                where -= 1
+            print(where)
+    if button_2.changed():
+        if button_2.official == 1:
+            print(where)
+    if button_3.changed():
+        if button_3.official == 1:
+            if where < 10:
+                where += 1
+            print(where)
