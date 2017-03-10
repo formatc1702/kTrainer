8679fbef02b4dab7a7bad6c92190bde05a20e0c8
Daniel Rojas <github@danielrojas.net>
2017-03-10 12:55:32 +0100

    Keep bitmap.bin always open

--- Firmware/bitmaps.py
+++ Firmware/bitmaps.py
@@ -14,22 +14,21 @@ SKIP     = const(12)
 SOUND    = const(13)
 X        = const(14)
 
-def load_bmp(pos):
-    f = open("bitmaps.bin","br")
-    f.seek(pos * 8)
-    return f.read(8)
-    f.close()
-
 class MyGFX:
     def __init__(self, pixel):
         self.pixel = pixel
+        self.f = open("bitmaps.bin","br")
 
     def draw_bitmap(self, bmp, x, y, color=1, size=8):
         for row, rowbyte in enumerate(bmp):
             for col in reversed(range(0,size)):
                 self.pixel(x+col,y+row,(rowbyte >> (size-col-1) & 0b1) ^ (1-color))
 
+    def load_bmp(self, pos):
+        self.f.seek(pos * 8)
+        return self.f.read(8)
+
     def button_icons3(self, icon1, icon2, icon3, row=24):
-        self.draw_bitmap(load_bmp(icon1),  0,row)
-        self.draw_bitmap(load_bmp(icon2), 60,row)
-        self.draw_bitmap(load_bmp(icon3),120,row)
+        self.draw_bitmap(self.load_bmp(icon1),  0,row)
+        self.draw_bitmap(self.load_bmp(icon2), 60,row)
+        self.draw_bitmap(self.load_bmp(icon3),120,row)
