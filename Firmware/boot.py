7aec936a0ea0aed2875c416fe23423948e52c0b0
Daniel Rojas <github@danielrojas.net>
2017-05-26 13:04:35 +0200

    Disable RGB LED at boot
    (because it's on the same pin as the on-board LED)

--- Firmware/boot.py
+++ Firmware/boot.py
@@ -1,3 +1,9 @@
+import machine, neopixel
+np = neopixel.NeoPixel(machine.Pin(2), 1)
+np[0]=(0,0,0)
+np.write() # turn off RGB LED
+
+
 import network
 ap = network.WLAN(network.AP_IF)
 ap.active(False) # disable WebREPL
