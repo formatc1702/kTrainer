b53a7cdffc114a090651083d5671a4e70d95602e
Daniel Rojas <github@danielrojas.net>
2018-06-24 17:17:00 +0200

    Upgrade to MicroPython 1.9.4

--- Firmware/_mpfshell-script.sh
+++ Firmware/_mpfshell-script.sh
@@ -6,7 +6,7 @@ ext=.py
 if [[ $cmd == *"c"* ]]; then
   if [[ $cmd != *"r"* ]]; then
     echo ⚙ Compile!
-    /Users/daniel/Projects/Utilities/ESP8266/Firmware/micropython-1.9.1/mpy-cross/mpy-cross $file$ext
+    /Users/daniel/Projects/Utilities/ESP8266/Firmware/micropython-1.9.4/mpy-cross/mpy-cross $file$ext
   fi
   ext=.mpy
 fi
